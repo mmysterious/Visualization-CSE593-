9898d3a7306154e927e6e30540732a903247f1c6
lkchen-x1c <knight0clk@hotmail.com>
2016-05-20 02:49:51 -0400

    finish query function

--- StockData/StoackMVC/Controllers/HomeController.cs
+++ StockData/StoackMVC/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.AspNet.Mvc;
+using StockMVC.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace StoackMVC.Controllers
+namespace StockMVC.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Query model)
+        {
+            return RedirectToAction("index", "table",
+                new { code = model.Code, from = model.From, to = model.To });
         }
     }
 }
---
+++ StockData/StoackMVC/Entities/Query.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockMVC.Entities
+{
+    public class Query
+    {
+        [Required]
+        public string Code { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
